3def8a0ba3b75e31c48e230ba07479c4efad8e71
Nicholas Davies <nicholasjohndavies42@gmail.com>
2022-10-03 13:57:01 +0800

    documentation

--- motor_code.c
+++ motor_code.c
@@ -1,3 +1,10 @@
+/** NOTE:  I have been uploading through the arduino IDE, unsure if certain 
+ *           headers need to be otherwise included 
+ *         used https://lastminuteengineers.com/l298n-dc-stepper-driver-arduino-tutorial/
+ *           for info on how to use the L298 driver.         
+ * */
+// 
+// for tutorial on how to use L298
 // naming conventions as per L298 diver specs.
 int enA = 5;
 int enB = 6;
@@ -5,12 +12,11 @@ int in1 = 4;
 int in2 = 7;
 
 void setup() {
-  // NOTE: enA, enB are connected to analog pins, can out anything from 0 to 1.
+  // NOTE: enA, enB are analog pins, can out anything from 0 to 255.
   pinMode(enA, OUTPUT);
   pinMode(enB, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
-  // initialise motors, off.
 }
 
 void forwards() {

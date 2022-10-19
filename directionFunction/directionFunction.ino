21442ea2d6e8e17f076c5999cf433e23ad01f56e
CodeWithTheDoctor <ashishithape.ai@gmail.com>
2022-10-19 12:43:18 +0800

    - added gps code

--- directionFunction/directionFunction.ino
+++ directionFunction/directionFunction.ino
@@ -1,6 +1,12 @@
 #include <TinyGPS++.h>
 #include <TinyGPSPlus.h>
 #include "SoftwareSerial.h"
+#include <math.h>
+
+//Configure the destination here
+
+#define LATITUDE 00.00
+#define LONGITUDE 00.00
 
 SoftwareSerial serial_connection(10, 9); //RX=pin 10, TX=pin 9 (Connect GPS modules RX to the Arduino's TX input and the modules TX to ardunio's RX input).
 TinyGPSPlus gps;//This is the GPS object that will pretty much do all the grunt work with the NMEA data
@@ -35,50 +41,67 @@ void setup()
   delay(5000);
   
 
-  //Logic to figure out direction of the vehicle
+  // Initialising variables for the car routing
+  double home[2] = [gps.satellites.lat(), gps.satellites.lng()];
+  double rC[2] = [LATITUDE, LONGITUDE]; 
+  double rA[2] = [C[0], C[1]];
 
+  // Todo: Code to Start driving forward
+  
 
 }
 
 void loop()
 {
-  while(serial_connection.available())//While there are characters to come from the GPS
+  while(serial_connection.available()) // While there is a connection between the nano and the gps module
   {
-    gps.encode(serial_connection.read());//This feeds the serial NMEA data into the library one char at a time
+    gps.encode(serial_connection.read()); //This feeds the serial NMEA data into the library one char at a time
 
   }
-  if(gps.location.isUpdated())//This will pretty much be fired all the time anyway but will at least reduce it to only after a package of NMEA data comes in
+  if(gps.location.isUpdated()) // If any gps data was collected
   {
-    //Get the latest info from the gps object which it derived from the data sent by the GPS unit
-    Serial.println("Satellite Count:");
-    Serial.println(gps.satellites.value());
-    Serial.println("Latitude:");
-    Serial.println(gps.location.lat(), 6);
-    Serial.println("Longitude:");
-    Serial.println(gps.location.lng(), 6);
-    Serial.println("Speed MPH:");
 
-    
-//    Serial.println(gps.speed.mph());
-//    Serial.println("Altitude Feet:");
-//    Serial.println(gps.altitude.feet());
-//    Serial.println("");
+    sleep(2000);
+    double rB[2] = [gps.satellites.lat(), gps.satellites.lng()];
+
+    double rAB[2] = [rB[0] - rA[0], rB[1] - rB[1]];
+    double rAC[2] = [rC[0] - rA[0], rC[1] - rA[1]];
+    double rBC[2] = [rC[0] - rB[0], rC[1] - rB[1]];
+
+    double a = sqrt(rAB[0]**2 + rAB[1]**2);
+    double b = sqrt(rBC[0]**2 + rBC[1]**2);
+    double c = sqrt(rAC[0]**2 + rAC[1]**2);
+
+    //Calculating the angle (in Radians)
+
+    double angle = acos((a**2 - b**2 + c**2)/(2*a*c));
+
+    // Update the rA value to that of current positon)
+    rA = [rB[0], rB[1]]; 
+
+    // Checking which manuever to execute based on angle and the location of the destination coordinate relative to current position, rB.
+    int angleFluctionThreshold = 0.0872665; // in Radians
+
+    if(angle < 0 + angleFluctionThreshold) {
+      continue;
+    }
+
+    else if (rAB[0] > rAC[0]) leftLean(2); 
+    // else if(rAB[1] > rAC[1]) rightLean(2);
+    else rightLean(2);
+
+
+
+
+
+    //Get the latest info from the gps object which it derived from the data sent by the GPS unit
+    // Serial.println("Satellite Count:");
+    // Serial.println(gps.satellites.value());
+    // Serial.println("Latitude:");
+    // Serial.println(gps.location.lat(), 6);
+    // Serial.println("Longitude:");
+    // Serial.println(gps.location.lng(), 6);
+    // Serial.println("Speed MPH:");
+
   }
 }
-
-/*
- * $GPRMC,183729,A,3907.356,N,12102.482,W,000.0,360.0,080301,015.5,E*6F
-$GPRMB,A,,,,,,,,,,,,V*71
-$GPGGA,183730,3907.356,N,12102.482,W,1,05,1.6,646.4,M,-24.1,M,,*75
-$GPGSA,A,3,02,,,07,,09,24,26,,,,,1.6,1.6,1.0*3D
-$GPGSV,2,1,08,02,43,088,38,04,42,145,00,05,11,291,00,07,60,043,35*71
-$GPGSV,2,2,08,08,02,145,00,09,46,303,47,24,16,178,32,26,18,231,43*77
-$PGRME,22.0,M,52.9,M,51.0,M*14
-$GPGLL,3907.360,N,12102.481,W,183730,A*33
-$PGRMZ,2062,f,3*2D
-$PGRMM,WGS 84*06
-$GPBOD,,T,,M,,*47
-$GPRTE,1,1,c,0*07
-$GPRMC,183731,A,3907.482,N,12102.436,W,000.0,360.0,080301,015.5,E*67
-$GPRMB,A,,,,,,,,,,,,V*71
-*/
--- motor_code.c
+++ motor_code.c
@@ -1,3 +1,15 @@
+#include <TinyGPS++.h>
+#include <TinyGPSPlus.h>
+#include "SoftwareSerial.h"
+#include <math.h>
+
+//Configure the destination here
+
+#define LATITUDE 00.00
+#define LONGITUDE 00.00
+
+
+
 /** NOTE:  I have been uploading through the arduino IDE, unsure if certain 
  *         headers need to be otherwise included 
  *         
@@ -15,16 +27,7 @@ int in4 = 7;
 int power = 100;
 
 
-void setup() {
-  // NOTE: enA, enB are connected to analog pins, can out anything from 0 to 1.
-  pinMode(enA, OUTPUT);
-  pinMode(enB, OUTPUT);
-  pinMode(in1, OUTPUT);
-  pinMode(in2, OUTPUT);
-  pinMode(in3, OUTPUT);
-  pinMode(in4, OUTPUT);
-  // initialise motors, off.
-}
+
 
 void forwards() {
   curve(2);
@@ -108,14 +111,115 @@ void rightLean(int amount) {
   delay(5000);
 }
 
-void loop() {
+void setup() {
+  // NOTE: enA, enB are connected to analog pins, can out anything from 0 to 1.
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  // initialise motors, off.
+
+
+  // ash code
+  Serial.begin(9600);//This opens up communications to the Serial monitor in the Arduino IDE
+  serial_connection.begin(9600);//This opens up communications to the GPS
+  Serial.println("GPS Start");//Just show to the monitor that the sketch has started
+  
+  pinMode(LED_BUILTIN, OUTPUT);
+
+// Flickers the inbuilt led until the GPS module is ready to send data to the nanu
+  while(!serial_connection.available()) {
+    digitalWrite(LED_BUILTIN, HIGH);
     delay(500);
-    for(int i = 0; i < 10; i++) {
-      rightLean(500);
-      stop();
-      delay(800);
-      leftLean(500);
-      stop();
-      delay(800); 
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
+  }
+
+  while(serial_connection.available()) {
+    // LED will turn on and off until data from satelite starts being sent.
+    if(gps.location.isUpdated()) {
+      break;
     }
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(2000);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(2000);
+  }
+
+  // Satellite connection established! Wait 5 seconds for satellite to stablize 
+  delay(5000);
+
+  // Initialising variables for the car routing
+  double home[2] = [gps.satellites.lat(), gps.satellites.lng()];
+  double rC[2] = [LATITUDE, LONGITUDE]; 
+  double rA[2] = [C[0], C[1]];
+
+  // Code to Start driving forward
+  forwards();
+}
+
+void loop() {
+    // delay(500);
+    // for(int i = 0; i < 10; i++) {
+    //   rightLean(500);
+    //   stop();
+    //   delay(800);
+    //   leftLean(500);
+    //   stop();
+    //   delay(800); 
+    // }
+
+  while(serial_connection.available()) // While there is a connection between the nano and the gps module
+  {
+    gps.encode(serial_connection.read()); //This feeds the serial NMEA data into the library one char at a time
+
+  }
+  if(gps.location.isUpdated()) // If any gps data was collected
+  {
+
+    delay(2000);
+    double rB[2] = [gps.satellites.lat(), gps.satellites.lng()];
+
+    double rAB[2] = [rB[0] - rA[0], rB[1] - rB[1]];
+    double rAC[2] = [rC[0] - rA[0], rC[1] - rA[1]];
+    double rBC[2] = [rC[0] - rB[0], rC[1] - rB[1]];
+
+    double a = sqrt(rAB[0]**2 + rAB[1]**2);
+    double b = sqrt(rBC[0]**2 + rBC[1]**2);
+    double c = sqrt(rAC[0]**2 + rAC[1]**2);
+
+    //Calculating the angle (in Radians)
+
+    double angle = acos((a**2 - b**2 + c**2)/(2*a*c));
+
+    // Update the rA value to that of current positon)
+    rA = [rB[0], rB[1]]; 
+
+    // Checking which manuever to execute based on angle and the location of the destination coordinate relative to current position, rB.
+    int angleFluctionThreshold = 0.0872665; // in Radians
+
+    if(angle < 0 + angleFluctionThreshold) {
+      continue;
+    }
+
+    else if (rAB[0] > rAC[0]) leftLean(500); 
+    // else if(rAB[1] > rAC[1]) rightLean(2);
+    else rightLean(500);
+
+
+
+
+
+    //Get the latest info from the gps object which it derived from the data sent by the GPS unit
+    // Serial.println("Satellite Count:");
+    // Serial.println(gps.satellites.value());
+    // Serial.println("Latitude:");
+    // Serial.println(gps.location.lat(), 6);
+    // Serial.println("Longitude:");
+    // Serial.println(gps.location.lng(), 6);
+    // Serial.println("Speed MPH:");
+
+  }
 }
